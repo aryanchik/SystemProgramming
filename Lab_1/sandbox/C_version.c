403d40a17569494ce27c0c73d53fb14f26d3a4df
aryanchik <shakharyan@yandex.ru>
2025-09-17 18:57:20 +0300

    Добавлена информация о лаб. 1

---
+++ Lab_1/sandbox/C_version.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Aryan\nShakh");
+    return 0;
+}
---
+++ Lab_1/sandbox/lab1.asm
@@ -0,0 +1,14 @@
+format ELF64
+public _start
+msg db "Shakh", 0xA, "Aryan", 0xA, 0
+
+_start:
+
+    mov rax, 4
+    mov rbx, 1
+    mov rcx, msg
+    mov rdx, 14
+    int 0x80
+    mov rax, 1
+    mov rbx, 0
+    int 0x80
