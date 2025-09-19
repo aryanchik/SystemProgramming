8cc74a9278b0e93f62a261a1c60c095a7b785c86
aryanchik <shakharyan@yandex.ru>
2025-09-19 09:11:08 +0300

    fixing 2

--- Lab_1/sandbox/lab1.asm
+++ Lab_1/sandbox/lab1.asm
@@ -1,13 +1,13 @@
 format ELF64
 public _start
-msg db "Shakh", 0xA, "Aryan", 0xA, 0
+msg db "Shakh", 0xA, "Aryan", 0xA, "Makhmudovich", 0xA, 0
 
 _start:
 
     mov rax, 4
     mov rbx, 1
     mov rcx, msg
-    mov rdx, 12
+    mov rdx, 25
     int 0x80
     mov rax, 1
     mov rbx, 0
